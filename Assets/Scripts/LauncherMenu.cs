b7c93a2f381c0a5024de15f2f4d9c0bb0e94e729
Paulrbo <paul.herbeau01@gmail.com>
2022-11-13 20:07:25 +0100

    difficulty_menu

--- Assets/Scripts/LauncherMenu.cs
+++ Assets/Scripts/LauncherMenu.cs
@@ -8,17 +8,29 @@ using UnityEngine.UI;
 public class LauncherMenu : MonoBehaviour
 {
     [SerializeField] private Button playButton;
+    [SerializeField] private Button difficultyButton;
     [SerializeField] private Button quitButton;
+    
+    public GameObject EnableDifficulty; //choisit l'objet à faire apparaitre
+    public GameObject DisableDifficulty; //choisit l'objet à faire disparaitre
+    
 
     private void Start()
     {
         playButton.onClick.AddListener(this.HandlePlayButton);
+        difficultyButton.onClick.AddListener(this.HandleDifficultyButton);
         quitButton.onClick.AddListener(this.HandleQuitButton);
     }
 
     private void HandlePlayButton()
     {
         SceneManager.LoadScene(1);
+    }
+    
+    private void HandleDifficultyButton()
+    {
+        EnableDifficulty.SetActive(true);
+        DisableDifficulty.SetActive(false);
     }
 
     private void HandleQuitButton()
